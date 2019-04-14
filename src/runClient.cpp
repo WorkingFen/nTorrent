13b8388e13bac54efd7250db82c2bcd78ea9991b
mbochene <mtbochenek@gmail.com>
2019-04-14 15:06:46 +0200

    Dodanie adresu IP i portu serwera do konstruktora klienta.

--- src/runClient.cpp
+++ src/runClient.cpp
@@ -5,18 +5,17 @@ using namespace std;
 int main()
 {
     try{
-        Client client1("127.0.0.1", 0);
+        Client client1("127.0.0.1", 0, "0.0.0.0");
         
         
-        Client client2("127.1.0.1", 0);
-        Client client3("127.2.0.1", 0);
+        Client client2("127.1.0.1", 0, "0.0.0.0");
+        Client client3("127.2.0.1", 0, "0.0.0.0");
         struct sockaddr_in x;
         socklen_t y= sizeof(x);
         getsockname(3, (struct sockaddr *) &x, &y);
         client2.connectTo(x);
         client3.connectTo(x);
         client1.run();
-        
         
     }
     catch(std::exception& e) { std::cerr << e.what() << std::endl;}
